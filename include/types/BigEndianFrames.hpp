c79cdbde23bface0aa85607937f15444d917a235
Simon Cahill <simon@h3lix.de>
2022-10-26 21:53:26 +0200

    Moved into separate namespace

--- include/types/BigEndianFrames.hpp
+++ include/types/BigEndianFrames.hpp
@@ -15,15 +15,16 @@
 #include <stdint.h>
 
 #include "types/FrameFlags.hpp"
+#include "types/FrameType.hpp"
 
-namespace isotpp { namespace types {
+namespace isotpp { namespace types { namespace frames {
 
     /**
      * @brief Provides the basis for all little endian frames.
      * 
      * @remarks This is the Protocol Control Information (PCI)
      */
-    struct BaseFrame {
+    struct BaseFrame_Struct {
         FrameType       frameType: 4; //!< The frame type
         uint8_t         reserved: 4; //!< First nybble of the message.
         uint8_t         reservedData[7]; //!< The rest of the CAN frame
@@ -34,7 +35,7 @@ namespace isotpp { namespace types {
      * 
      * @remarks A single frame has max. 7 data bytes and 1 control byte, containing the data length and frame type.
      */
-    struct SingleFrame {
+    struct SingleFrame_Struct {
         FrameType       frameType: 4; //!< The second nybble of the message. Equal to SINGLE_FRAME.
         uint8_t         dataLength: 4; //!< First nybble of the message, containing the data length (0-7)
         uint8_t         frameData[7]; //!< The actual data sent with the frame
@@ -48,7 +49,7 @@ namespace isotpp { namespace types {
      * dedicated to the actual data length.
      * This means that the first frame can only transmit 6 bytes of actual data.
      */
-    struct FirstFrame {
+    struct FirstFrame_Struct {
         FrameType       frameType: 4; //!< The frame type. Equal to FIRST_FRAME.
         uint8_t         dataLengthHigh: 4; //!< First nybble of the message containing the four high-bits of the data length
         uint8_t         dataLengthLow; //!< The low 8 bits of the data length (equaling 12 bits total length)
@@ -60,7 +61,7 @@ namespace isotpp { namespace types {
      * 
      * These frames contain up to 7 bytes of data.
      */
-    struct ConsecutiveFrame {
+    struct ConsecutiveFrame_Struct {
         FrameType       frameType: 4; //!< Second nybble of the message. Equal to CONSECUTIVE_FRAME
         uint8_t         frameIndex: 4; //!< First nybble of the message. Contains the frame's index.
         uint8_t         frameData[7]; //!< The next load of data sent with this frame
@@ -69,14 +70,14 @@ namespace isotpp { namespace types {
     /**
      * @brief Flow-control frame. These frames tell the server how to proceed with the transmission.
      */
-    struct FlowControlFrame {
+    struct FlowControlFrame_Struct {
         FrameType       frameType: 4; //!< The frame type. Equal to FLOW_CONTROL_FRAME.
         FlowControlFlag flowControlFlag: 4; //!< First nybble of the message. Contains the flow control flags (continue, wait, overflow/abort)
         uint8_t         blockSize; //!< The amount of frames to send before next flow-control frame.
         uint8_t         frameSeparationTime; //!< The separation time (in ms) between sending frames. <= 127 = millis. 0xf1 - 0xf9 100 to 900 microseconds
     } __attribute__((packed));
 
-} /* namespace types */ } /* namespace isotpp */
+} /* namespace frames */ } /* namespace types */ } /* namespace isotpp */
 
 #endif // ISOTPP_INCLUDE_TYPES_BIGENDIANFRAMES_HPP
 #endif // BYTE_ORDER == LITTLE_ENDIAN
